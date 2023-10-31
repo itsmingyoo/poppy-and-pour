50547391d72f3939d9f7016bd5efdeeef24e0a86
SebastianStovall <sebastianstovall@gmail.com>
2023-10-31 14:28:10 -0500

    changed package.json

--- README.md
+++ README.md
@@ -39,11 +39,3 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 # poppy-and-pour
-
-
-
-
-
-<!-- "dev": "next dev",
-"build": "next build",
-"start": "next start", -->
--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-
-const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
-const dotenv = require('dotenv')
-
-dotenv.config({
-  path: `.env.${process.env.NODE_ENV}`
-})
-
-module.exports = (phase, {defaultConfig}) => {
-  return {
-    env: {
-      DATABASE_URL: process.env.DATABASE_URL,
-      SCHEMA: process.env.SCHEMA
-    }
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
