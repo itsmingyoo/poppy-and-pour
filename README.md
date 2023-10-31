0f89158113f8af0bb1c2c849057b30e0ac22c3c3
SebastianStovall <sebastianstovall@gmail.com>
2023-10-31 14:09:49 -0500

    setting up prisma, made next.config file for local/production env's

--- README.md
+++ README.md
@@ -39,3 +39,11 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 # poppy-and-pour
+
+
+
+
+
+<!-- "dev": "next dev",
+"build": "next build",
+"start": "next start", -->
--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
+const dotenv = require('dotenv')
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
+module.exports = (phase, {defaultConfig}) => {
+  return {
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL,
+      SCHEMA: process.env.SCHEMA
+    }
+  }
+}
