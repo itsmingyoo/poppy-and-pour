0b94a133faedd81007bdf28f37bedbfc69c101f3
Minh Tran <minhttran93@gmail.com>
2023-11-02 11:27:49 -0700

    add route for product details page using getStaticProps and getStaticPaths

--- pages/api/products/[productId].js
+++ pages/api/products/[productId].js
@@ -1,5 +1,32 @@
-import { prisma } from '../../../server/db/client'
+import { prisma } from "../../../server/db/client";
 
 async function handler(req, res) {
+  const productId = req.query.productId;
+  const product = await getProductDetails(productId);
 
+  if (!product)
+    return res.status(500).json({ message: "Error retrieving product" });
+
+  console.log("this is product in the backend handler", product);
+
+  res.status(200).json(product); // this returns the product and status
 }
+
+export async function getProductDetails(productId) {
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: parseInt(productId),
+      },
+    });
+    console.log("this is the product in the helper fn", product);
+    return product;
+  } catch (e) {
+    console.error(e);
+    return null;
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export default handler;
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,27 +1,27 @@
-import { prisma } from '../../../server/db/client'
+import { prisma } from "../../../server/db/client";
 
 async function handler(req, res) {
-    const products = await getAllProducts()
-    console.log("PRODUCTS ----> ", products)
-    res.status(200).json(products)
+  const products = await getAllProducts();
+
+  if (!products)
+    return res.status(500).json({ message: "Error retrieving products" });
+
+  console.log("PRODUCTS ----> ", products);
+
+  res.status(200).json(products);
 }
-
-
 
 export async function getAllProducts() {
-
-    try {
-        const products = await prisma.product.findMany();
-        return products
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Something Went Wrong Retreiving Products' });
-        return null
-    } finally {
-        await prisma.$disconnect();
-    }
-
+  try {
+    const products = await prisma.product.findMany();
+    return products;
+  } catch (error) {
+    console.error(error);
+    // res.status(500).json({ error: "Something Went Wrong Retreiving Products" });
+    return null;
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-
-export default handler
+export default handler;
