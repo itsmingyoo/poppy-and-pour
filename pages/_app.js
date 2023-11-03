9559181c92ac3d5124d5afdff1d16f7de58e4101
SebastianStovall <sebastianstovall@gmail.com>
2023-11-02 19:06:01 -0500

    added layout component and navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import Layout from '@/components/layout/layout';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     // `session` comes from `getServerSideProps` or `getInitialProps`.
     // Avoids flickering/session loading on first load.
     <SessionProvider session={session} refetchInterval={5 * 60}>
+      <Layout>
         <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,14 @@ import Link from "next/link";
 
 function HomePage() {
 
-  function logoutHandler() {
-    signOut()
-  }
-
-
-
   return (
-
-    
     <div>
       <h1>THE HOME PAGE</h1>
 
-      <Link href='/auth'>login/signup form YO</Link>
-      <br />
       <Link href='/products'> OUR PRODUCTS (Leads to reviews) </Link>
       <br />
       <Link href='/categories'> OUR CATEGORIES </Link>
       <br />
-      <button onClick={logoutHandler}> !!!! CLICK TO LOGOUT USER !!!! </button>
     </div>
   );
 }
