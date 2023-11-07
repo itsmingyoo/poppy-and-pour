49e565bf5d7a2261c37917b27a5738850a64bbbc
SebastianStovall <sebastianstovall@gmail.com>
2023-11-06 19:27:24 -0600

    added get route for orders

--- pages/orders/index.js
+++ pages/orders/index.js
@@ -1,29 +1,52 @@
-import { useSession } from "next-auth/react"
+import { getSession } from 'next-auth/react'
+import { getAllOrders } from '../api/orders/index'
 
-function Orders() {
-    const { data: session, status } = useSession()
-    const loading = status === "loading"
+function Orders({orders}) {
 
-    if(session) {
-        console.log(session)  //<------- holds data for the active session (null if no authenticated user)
-    }
-
-    async function displayUserOrdersHandler() {
-        const response = await fetch('/api/orders/', {
-            method: 'POST',
-            body: JSON.stringify(session),
-            headers: { 'Content-Type': 'application/json' }
-        })
-    }
+    if(orders.length === 0) return <h2 style={{textAlign: 'center'}}>This User Has No Active Orders</h2>
 
     return (
         <>
             <h1>USER ORDERS</h1>
-            <p>Click the button below to display the user's orders</p>
-            <button onClick={displayUserOrdersHandler}>GET ORDERS</button>
-            {session && console.log("SESSION", session)}
+            {orders && orders.map((order) => {
+                return <div key={order.id}>
+                    <p>ORDER STATUS: {order.status}</p>
+                    <p>TRACKING NUMBER: {order.trackingNumber}</p>
+                    <p>ORDER DATE: {order.orderDate}</p>
+                    <p>BATCH ID: {order.batchId}</p>
+                    <p>TOTAL: {order.total}</p>
+                </div>
+            })}
         </>
     )
 }
+
+// we use getServerSideProps() here since display Orders is a protected component
+export async function getServerSideProps(context) {
+    console.log("GRABBING LATEST DATA FROM BACKEND...")
+    const session = await getSession({req: context.req})
+
+    if(!session) {
+        console.log("THERE IS NO SESSION")
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false
+            }
+        }
+    }
+
+    const userId = session.user.userId
+    const allOrders = await getAllOrders(userId)
+
+    return {
+        props: {
+            orders: allOrders,
+            session
+        }
+    }
+}
+
+
 
 export default Orders
