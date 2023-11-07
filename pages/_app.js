e42973ca437486314706fb72c6544d70e7571bb4
JLaikows <joshua@laikowski.com>
2023-11-06 20:33:29 -0500

    Added toastify for login functions

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,20 @@
-import "@/styles/globals.css";
-import { SessionProvider } from "next-auth/react";
-import Layout from "@/components/layout/layout";
+import '@/styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
+import Layout from '@/components/layout/layout'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    // `session` comes from `getServerSideProps` or `getInitialProps`.
-    // Avoids flickering/session loading on first load.
-    <SessionProvider session={session} refetchInterval={5 * 60}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
-  );
+    return (
+        // `session` comes from `getServerSideProps` or `getInitialProps`.
+        // Avoids flickering/session loading on first load.
+        <SessionProvider session={session} refetchInterval={5 * 60}>
+            <Layout>
+                <ToastContainer position="bottom-left" />
+                <Component {...pageProps} />
+            </Layout>
+        </SessionProvider>
+    )
 }
 
-export default MyApp;
-
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
+export default MyApp
