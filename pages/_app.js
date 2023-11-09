2737c0086b41b3feb023c300e264545a9f096697
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-11-09 13:57:31 -0500

    add theme provider and set primiary color for example usage

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,29 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import Layout from "@/components/layout/layout";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+
+  //   dark = false
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#EBA392"
+      }
+    }
+  })
+
+
   return (
     // `session` comes from `getServerSideProps` or `getInitialProps`.
     // Avoids flickering/session loading on first load.
     <SessionProvider session={session} refetchInterval={5 * 60}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
