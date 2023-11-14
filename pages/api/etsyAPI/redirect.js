57f41d36241e857dc8f1a202a4eec65445127494
SebastianStovall <sebastianstovall@gmail.com>
2023-11-14 16:26:26 -0600

    put hash PCKE inside redirect handler

--- pages/api/etsyAPI/redirect.js
+++ pages/api/etsyAPI/redirect.js
@@ -2,7 +2,7 @@
 // in the initial authorization request, and the client verifier compliment
 // to the code_challenge sent with the initial authorization request
 
-// const clientID = process.env.ETSY_API_KEY
+const clientID = process.env.ETSY_API_KEY
 // const clientVerifier = 'm6L-429KzxMb7eJ-j55389wFNCELwJ0MNzWfB6M-_-4'
 // const redirectUri = 'https://www.etsy.com/oauth/connect?response_type=code&redirect_uri=http://localhost:3000/oauth/redirect&scope=email_r&client_id=1aa2bb33c44d55eeeeee6fff&state=naofo6&code_challenge=6VYHYyQUZg81Bo5ZTjqi4iWYRigSrLOHwlcv3unJAMk&code_challenge_method=S256'
 import { generateRedirectURI } from "@/lib/PKCE"
@@ -10,18 +10,9 @@ import { generateRedirectURI } from "@/lib/PKCE"
 // to this route. The authorization code is in the `code` param
 async function handler(req, res) {
 
-    const redirectUri = generateRedirectURI()
-
-    const uriResponse = await fetch(redirectUri)
-    if(uriResponse.ok) {
-        const data = uriResponse.json()
-        console.log("DATA BEFORE PARSE ", data)
-        const {uri, clientVerifier} = data
-        console.log("AUTH CODEEEEEEEEEEEEEEEEE ----> ", uri)
-        console.log('CLIENT VERIFYER ---> ', clientVerifier)
-    } else {
-        console.log("FUCK YOU")
-    }
+    const {redirect_uri, clientVerifier} = generateRedirectURI()
+    console.log("URI ---> ", uri)
+    console.log("CLIENT VER ---> ", clientVerifier)
 
     const tokenUrl = 'https://api.etsy.com/v3/public/oauth/token'
     const requestOptions = {
@@ -29,7 +20,7 @@ async function handler(req, res) {
         body: JSON.stringify({
             grant_type: 'authorization_code',
             client_id: clientID,
-            redirect_uri: redirectUri,
+            redirect_uri,
             code: authCode,
             code_verifier: clientVerifier,
         }),
