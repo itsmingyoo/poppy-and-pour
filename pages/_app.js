ff9ebde4ccaf02bba9fbf0bb4ec349ee387fcc43
Chris <cjthornburg@thornburgsfamily.com>
2023-11-15 10:47:47 -0500

    font added

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,17 @@ import { SessionProvider } from "next-auth/react";
 import Layout from "@/components/layout/layout";
 import { createTheme, ThemeProvider } from "@mui/material";
 
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 
   //   dark = false
+
   const theme = createTheme({
-    
+    typography: {
+      fontFamily: '"Inria Sans", sans-serif' , // Replace 'Your-Primary-Font' with the desired font
+    },
     palette: {
+
       primary: {
         main: "#EBA392"
       }
@@ -20,6 +25,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     // `session` comes from `getServerSideProps` or `getInitialProps`.
     // Avoids flickering/session loading on first load.
+
     <SessionProvider session={session} refetchInterval={5 * 60}>
       <ThemeProvider theme={theme}>
         <Layout>
