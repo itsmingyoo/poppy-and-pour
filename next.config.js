2ffb29e219b344aa227d63c6b786de90110a89ff
Minh Tran <minhttran93@gmail.com>
2023-11-20 21:16:46 -0800

    auth_url works with env variables

--- next.config.js
+++ next.config.js
@@ -7,21 +7,23 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    isrMemoryCacheSize: 0, // cache size in bytes
-  },
-  env: {
-    SHOPIFY_STOREFRONT_ACCESS_TOKEN:
-    process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
-    SHOPIFY_ADMIN_ACCESS_TOKEN:
-    process.env.SHOPIFY_ADMIN_ACCESS_TOKEN,
-    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-    ETSY_API_KEY: process.env.ETSY_API_KEY,
-    CODE_CHALLENGE: process.env.CODE_CHALLENGE,
-    STATE: process.env.STATE,
-    SHOP_ID: process.env.SHOP_ID
-  },
-};
+    reactStrictMode: true,
+    experimental: {
+        isrMemoryCacheSize: 0, // cache size in bytes
+    },
+    env: {
+        SHOPIFY_STOREFRONT_ACCESS_TOKEN:
+            process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+        SHOPIFY_ADMIN_ACCESS_TOKEN: process.env.SHOPIFY_ADMIN_ACCESS_TOKEN,
+        SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
+        SHOPIFY_API_SECRET_KEY: process.env.SHOPIFY_API_SECRET_KEY,
+        NONCE: process.env.NONCE,
+        SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+        ETSY_API_KEY: process.env.ETSY_API_KEY,
+        CODE_CHALLENGE: process.env.CODE_CHALLENGE,
+        STATE: process.env.STATE,
+        SHOP_ID: process.env.SHOP_ID,
+    },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- pages/api/shopify/createProduct.js
+++ pages/api/shopify/createProduct.js
@@ -1,24 +1,17 @@
-
 // https://poppy-and-pour.myshopify.com/admin/api/2023-10/graphql.json
-// const {getAllProducts2} = require('../../../lib/shopify)
-import { getAllProducts2 } from "@/lib/shopify";
+import { getAllProducts2 } from '@/lib/shopify'
 
-async function handler (req, res) {
-
-
-        const response = await getAllProducts2()
-        if(response.ok) {
-            const data = await response.json()
-            return res.status(201).json(data)
-        } else {
-            console.log(response)
-            return res.status(500).json(response)
-        }
-
-
-
+async function handler(req, res) {
+    const state = Shopify.auth.nonce()
+    console.log('state', state)
+    const response = await getAllProducts2()
+    if (response.ok) {
+        const data = await response.json()
+        return res.status(201).json(data)
+    } else {
+        console.log(response)
+        return res.status(500).json(response)
+    }
 }
 
-
-
-export default handler;
+export default handler
