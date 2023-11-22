a90afa21a38de2f67672987ccce06e3a53fe64cd
Cjthornburg <Cjthornburg@thornburgsfamily.com>
2023-11-22 16:51:02 -0500

    add products, need scope change approval

--- pages/api/shopify/createProduct.js
+++ pages/api/shopify/createProduct.js
@@ -1,17 +1,47 @@
-// https://poppy-and-pour.myshopify.com/admin/api/2023-10/graphql.json
-import { getAllProducts2 } from '@/lib/shopify'
+
+const shopifyAPI = require('shopify-node-api')
+import {config} from "../../../lib/shopifyConfig"
 
 async function handler(req, res) {
-    const state = Shopify.auth.nonce()
-    console.log('state', state)
-    const response = await getAllProducts2()
-    if (response.ok) {
-        const data = await response.json()
-        return res.status(201).json(data)
-    } else {
-        console.log(response)
-        return res.status(500).json(response)
-    }
+
+    const shopify = new shopifyAPI(config)
+
+    var post_data = {
+        "product": {
+          "title": "test product 1",
+          "body_html": "<strong>Good snowboard!</strong>",
+          "vendor": "Poppy and Pour",
+          "product_type": "Snowboard",
+          "variants": [
+            {
+              "option1": "First",
+              "price": "10.00",
+              "sku": 123
+            },
+            {
+              "option1": "Second",
+              "price": "20.00",
+              "sku": "123"
+            }
+          ]
+        }
+      }
+
+
+
+          shopify.post('/admin/products.json', post_data, function(err, data, headers) {
+            if(err){
+
+                console.log("error message ", err)
+            } else {
+
+                console.log("data message", data)
+            }
+          });
+
+
+
+ return res.status(200).json(null)
 }
 
 export default handler
