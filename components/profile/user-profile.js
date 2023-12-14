ca96ca4cd9bf74fc5d3299eec7a40bfbda965f52
Minh Tran <minhttran93@gmail.com>
2023-12-14 04:22:36 -0800

    add profile mit

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,53 +1,57 @@
-import { getSession } from "next-auth/react"
+import { getSession } from 'next-auth/react'
 import { useState, useEffect } from 'react'
-import ProfileForm from './profile-form';
+import ProfileForm from './profile-form'
+import Dashboard from '../dashboard/dashboard'
 
 function UserProfile() {
-
-    const [formErrors, setFormErrors] = useState({});
-    const [successMessage, setSuccessMessage] = useState("")
+    const [formErrors, setFormErrors] = useState({})
+    const [successMessage, setSuccessMessage] = useState('')
 
     async function changePasswordHandler(passwordObjData) {
         // attach session data to passwordObjData
         const session = await getSession()
-        passwordObjData["session"] = session
+        passwordObjData['session'] = session
 
         const response = await fetch('/api/user/change-password', {
             method: 'PATCH',
             body: JSON.stringify(passwordObjData),
             headers: {
-                'Content-Type': 'application/json'
-            }
+                'Content-Type': 'application/json',
+            },
         })
 
         const data = await response.json()
-        console.log("FRONTEND RESPONSE -----> ", data)
+        console.log('FRONTEND RESPONSE -----> ', data)
 
         if (response.ok) {
             // Password change was successful; you can clear any form-specific errors here
-            setFormErrors({});
+            setFormErrors({})
 
-            setSuccessMessage("Successfully Changed Password!")
+            setSuccessMessage('Successfully Changed Password!')
             const timer = setTimeout(() => {
-                setSuccessMessage("");
-            }, 3000);
+                setSuccessMessage('')
+            }, 3000)
             return () => {
-                clearTimeout(timer);
-            };
-
-            } else {
+                clearTimeout(timer)
+            }
+        } else {
             // Password change had errors; update the form-specific errors state
-            setFormErrors(data.errors || {});
+            setFormErrors(data.errors || {})
         }
-
-
     }
 
     return (
-        <section>
-            <ProfileForm onChangePassword={changePasswordHandler} backendFormErrors={formErrors} successMessage={successMessage} />
-        </section>
-    );
+        <>
+            <section>
+                <Dashboard />
+                <ProfileForm
+                    onChangePassword={changePasswordHandler}
+                    backendFormErrors={formErrors}
+                    successMessage={successMessage}
+                />
+            </section>
+        </>
+    )
 }
 
-export default UserProfile;
+export default UserProfile
